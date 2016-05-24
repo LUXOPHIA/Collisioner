1acae0db78bc705bc596d8272e09105500e0baf7
luxidea <contact@luxidea.net>
2016-05-24 14:07:45 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,102 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.StdCtrls, FMX.Controls.Presentation, FMX.Objects, FMX.Edit,
+  LUX.Matrix.L4, LUX.Raytrace, LUX.Raytrace.Geometry, LUX.Raytrace.Material, LUX.Raytrace.Render;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Panel1: TPanel;
+    GroupBoxI: TGroupBox;
+    LabelIW: TLabel;
+    EditIW: TEdit;
+    LabelIH: TLabel;
+    EditIH: TEdit;
+    GroupBoxR: TGroupBox;
+    ButtonP: TButton;
+    ButtonS: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure ButtonPClick(Sender: TObject);
+    procedure ButtonSClick(Sender: TObject);
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+    _Render :TRender;
+    _World  :TWorld;
+    _Sphere :TSphere;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+   N :Integer;
+begin
+     _Render := TRender.Create;
+
+     _World  := TWorld.Create;
+
+     _Render.Scene := _World;
+
+     for N := 1 to 100 do
+     begin
+          with TSphere.Create do
+          begin
+               Paren       := _World;
+               Radius      := Random;
+
+               LocalMatrix := LocalMatrix * TSingleM4.Translate( 8 * Random - 4, 6 * Random - 3, 6 * Random - 3 );
+          end;
+     end;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+     _World.Free;
+
+     _Render.Free;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.ButtonPClick(Sender: TObject);
+begin
+     ButtonP.Enabled := False;
+     ButtonS.Enabled := True ;
+
+     Image1.Bitmap.SetSize( 640, 480 );
+
+     _Render.Run;
+
+     _Render.CopyToBitmap( Image1.Bitmap );
+
+     ButtonP.Enabled := True ;
+     ButtonS.Enabled := False;
+end;
+
+procedure TForm1.ButtonSClick(Sender: TObject);
+begin
+     ButtonS.Enabled := False;
+
+     _Render.Stop;
+end;
+
+end. //######################################################################### ■
