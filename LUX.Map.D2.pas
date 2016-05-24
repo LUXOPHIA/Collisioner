b121c6565dc0da0c95f620271fc0f4a5c8fb5851
luxidea <contact@luxidea.net>
2016-05-24 14:01:43 +0900

    Bric/GridArray を追加。

--- LUX.Map.D2.pas
+++ LUX.Map.D2.pas
@@ -38,14 +38,56 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        procedure AfterConstruction; override;
        destructor Destroy; override;
        ///// プロパティ
-       property CountX                      :Integer read _CountX  write SetCountX;
-       property CountY                      :Integer read _CountY  write SetCountY;
-       property MarginX                     :Integer read _MarginX write SetMarginX;
-       property MarginY                     :Integer read _MarginY write SetMarginY;
-       property Item[ const X_,Y_:Integer ] :T_Item  read GetItem  write SetItem;    default;
+       property CountX                      :Integer read   _CountX  write SetCountX ;
+       property CountY                      :Integer read   _CountY  write SetCountY ;
+       property MarginX                     :Integer read   _MarginX write SetMarginX;
+       property MarginY                     :Integer read   _MarginY write SetMarginY;
+       property Item[ const X_,Y_:Integer ] :T_Item  read GetItem    write SetItem   ; default;
        ///// メソッド
        class procedure Swap( var Array0_,Array1_:TArray2D<T_Item> ); static;
        procedure MakeEdgeLoop;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TBricArray2D<T_Item>
+
+     TBricArray2D<T_Item> = class( TArray2D<T_Item> )
+     private
+     protected
+       ///// アクセス
+       function GetGridX :Integer;
+       procedure SetGridX( const GridX_:Integer );
+       function GetGridY :Integer;
+       procedure SetGridY( const GridY_:Integer );
+     public
+       ///// プロパティ
+       property Bric[ const X_,Y_:Integer ] :T_Item  read GetItem   write SetItem  ; default;
+       property BricX                    :Integer read   _CountX write SetCountX;
+       property BricY                    :Integer read   _CountY write SetCountY;
+       property GridX                    :Integer read GetGridX  write SetGridX ;
+       property GridY                    :Integer read GetGridY  write SetGridY ;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGridArray2D<T_Item>
+
+     TGridArray2D<T_Item> = class( TArray2D<T_Item> )
+     private
+     protected
+       ///// アクセス
+       function GetBricX :Integer;
+       procedure SetBricX( const BricX_:Integer );
+       function GetBricY :Integer;
+       procedure SetBricY( const BricY_:Integer );
+     public
+       constructor Create; overload;
+       constructor Create( const BricX_,BricY_:Integer ); overload;
+       constructor Create( const BricX_,BricY_,MarginX_,MarginY_:Integer ); overload;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Grid[ const X_,Y_:Integer ] :T_Item  read GetItem   write SetItem  ; default;
+       property GridX                    :Integer read   _CountX write SetCountX;
+       property GridY                    :Integer read   _CountY write SetCountY;
+       property BricX                    :Integer read GetBricX  write SetBricX ;
+       property BricY                    :Integer read GetBricY  write SetBricY ;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -188,6 +230,90 @@ begin
           for EX :=  0        to _CountX         -1 do Item[ EX, EY ] := Item[ EX          , MY ];
           for EX :=  _CountX  to _CountX+_MarginX-1 do Item[ EX, EY ] := Item[ EX - _CountX, MY ];
      end;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TBricArray2D<T_Item>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TBricArray2D<T_Item>.GetGridX :Integer;
+begin
+     Result := BricX + 1;
+end;
+
+procedure TBricArray2D<T_Item>.SetGridX( const GridX_:Integer );
+begin
+     BricX := GridX_ - 1;
+end;
+
+function TBricArray2D<T_Item>.GetGridY :Integer;
+begin
+     Result := BricY + 1;
+end;
+
+procedure TBricArray2D<T_Item>.SetGridY( const GridY_:Integer );
+begin
+     BricY := GridY_ - 1;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGridArray2D<T_Item>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TGridArray2D<T_Item>.GetBricX :Integer;
+begin
+     Result := _CountX - 1;
+end;
+
+procedure TGridArray2D<T_Item>.SetBricX( const BricX_:Integer );
+begin
+     _CountX  := BricX_ + 1;  MakeArray;
+end;
+
+function TGridArray2D<T_Item>.GetBricY :Integer;
+begin
+     Result := _CountY - 1;
+end;
+
+procedure TGridArray2D<T_Item>.SetBricY( const BricY_:Integer );
+begin
+     _CountY  := BricY_ + 1;  MakeArray;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGridArray2D<T_Item>.Create;
+begin
+     Create( 0, 0 );
+end;
+
+constructor TGridArray2D<T_Item>.Create( const BricX_,BricY_:Integer );
+begin
+     Create( BricX_, BricY_, 0, 0 );
+end;
+
+constructor TGridArray2D<T_Item>.Create( const BricX_,BricY_,MarginX_,MarginY_:Integer );
+begin
+     inherited;
+
+     _CountX  := BricX_ + 1;
+     _CountY  := BricY_ + 1;
+end;
+
+destructor TGridArray2D<T_Item>.Destroy;
+begin
+
+     inherited;
 end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
