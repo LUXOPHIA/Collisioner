09e8c7bb637365aa13275bcbe62ebed619cae7f5
luxidea <contact@luxidea.net>
2016-10-30 09:26:31 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX.Geometry/' changes from 7b1cc82..b581713
    b581713 2D Shape Matching 法の行列計算関数 ShapeMatch を追加。
    138ea41 Merge commit '47b6e3771064230004ac27485cf69235cd3d6524' into nakayama
    47b6e37 Squashed '_LIBRARY/LUXOPHIA/LUX.Brep/' changes from 8cd0156..9c5a0da
    619595b Merge commit '3bda2b948440a8c7943d82e97e78926cc2a6c200' into nakayama
    29c47a4 開/閉曲線の長さを求める関数 CurveLength / ClosedCurveLength 関数を追加。 点群を包むバウンディングボックスを求める BoundingBox 関数を追加。
    9e60042 CircumCenter 関数の制度改善。
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX.Geometry
    git-subtree-split: b5817135ed18e4cdc1fcd7f21a6e2a600f220fd8

--- LUX.Geometry.D2.pas
+++ LUX.Geometry.D2.pas
@@ -2,7 +2,7 @@
 
 interface //#################################################################### ■
 
-uses LUX, LUX.D2, LUX.Geometry;
+uses LUX, LUX.D2, LUX.Geometry, LUX.Matrix.L2;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
@@ -90,7 +90,21 @@ function InnerCenter( const P1_,P2_,P3_:TDoublePos2D ) :TDoublePos2D; overload;
 function CircumCenter( const P1_,P2_,P3_:TSingle2D ) :TSingle2D; overload;
 function CircumCenter( const P1_,P2_,P3_:TDouble2D ) :TDouble2D; overload;
 
+function CurveLength( const Ps_:array of TSingle2D ) :Single; overload;
+function CurveLength( const Ps_:array of TDouble2D ) :Single; overload;
+
+function ClosedCurveLength( const Ps_:array of TSingle2D ) :Single; overload;
+function ClosedCurveLength( const Ps_:array of TDouble2D ) :Single; overload;
+
+function BoundingBox( const Ps_:array of TSingle2D ) :TSingleArea2D; overload;
+function BoundingBox( const Ps_:array of TDouble2D ) :TDoubleArea2D; overload;
+
+function ShapeMatch( const Vs0_,Vs1_:array of TSingle2D ) :TSingleM2; overload;
+function ShapeMatch( const Vs0_,Vs1_:array of TDouble2D ) :TDoubleM2; overload;
+
 implementation //############################################################### ■
+
+uses System.SysUtils;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
@@ -327,14 +341,9 @@ begin
      L2 := P2_.Siz2;  E2 := P1_ - P3_;
      L3 := P3_.Siz2;  E3 := P2_ - P1_;
 
-     W := 2 * ( E2.X * E1.Y - E1.X * E2.Y );
-     {
-     W := 2 * ( E3.X * E2.Y - E2.X * E3.Y );
-     W := 2 * ( E1.X * E3.Y - E3.X * E1.Y );
      W := 2 * ( P2_.X * P1_.Y - P1_.X * P2_.Y
               + P3_.X * P2_.Y - P2_.X * P3_.Y
               + P1_.X * P3_.Y - P3_.X * P1_.Y );
-     }
 
      with Result do
      begin
@@ -352,20 +361,179 @@ begin
      L2 := P2_.Siz2;  E2 := P1_ - P3_;
      L3 := P3_.Siz2;  E3 := P2_ - P1_;
 
-     W := 2 * ( E2.X * E1.Y - E1.X * E2.Y );
-     {
-     W := 2 * ( E3.X * E2.Y - E2.X * E3.Y );
-     W := 2 * ( E1.X * E3.Y - E3.X * E1.Y );
      W := 2 * ( P2_.X * P1_.Y - P1_.X * P2_.Y
               + P3_.X * P2_.Y - P2_.X * P3_.Y
               + P1_.X * P3_.Y - P3_.X * P1_.Y );
-     }
 
      with Result do
      begin
           X := ( L1 * E1.Y + L2 * E2.Y + L3 * E3.Y ) / +W;
           Y := ( L1 * E1.X + L2 * E2.X + L3 * E3.X ) / -W;
      end;
+end;
+
+//------------------------------------------------------------------------------
+
+function CurveLength( const Ps_:array of TSingle2D ) :Single;
+var
+   I :Integer;
+   P0, P1 :TSingle2D;
+begin
+     Result := 0;
+
+     P1 := Ps_[ 0 ];
+     for I := 1 to High( Ps_ ) do
+     begin
+          P0 := P1;  P1 := Ps_[ I ];
+
+          Result := Result + Distance( P0, P1 );
+     end;
+end;
+
+function CurveLength( const Ps_:array of TDouble2D ) :Single;
+var
+   I :Integer;
+   P0, P1 :TDouble2D;
+begin
+     Result := 0;
+
+     P1 := Ps_[ 0 ];
+     for I := 1 to High( Ps_ ) do
+     begin
+          P0 := P1;  P1 := Ps_[ I ];
+
+          Result := Result + Distance( P0, P1 );
+     end;
+end;
+
+//------------------------------------------------------------------------------
+
+function ClosedCurveLength( const Ps_:array of TSingle2D ) :Single;
+var
+   I :Integer;
+   P0, P1, P2 :TSingle2D;
+begin
+     Result := 0;
+
+     P2 := Ps_[ 0 ];
+
+     P1 := P2;
+     for I := 1 to High( Ps_ ) do
+     begin
+          P0 := P1;  P1 := Ps_[ I ];
+
+          Result := Result + Distance( P0, P1 );
+     end;
+
+     Result := Result + Distance( P1, P2 );
+end;
+
+function ClosedCurveLength( const Ps_:array of TDouble2D ) :Single;
+var
+   I :Integer;
+   P0, P1, P2 :TDouble2D;
+begin
+     Result := 0;
+
+     P2 := Ps_[ 0 ];
+
+     P1 := P2;
+     for I := 1 to High( Ps_ ) do
+     begin
+          P0 := P1;  P1 := Ps_[ I ];
+
+          Result := Result + Distance( P0, P1 );
+     end;
+
+     Result := Result + Distance( P1, P2 );
+end;
+
+//------------------------------------------------------------------------------
+
+function BoundingBox( const Ps_:array of TSingle2D ) :TSingleArea2D;
+var
+   P :TSingle2D;
+begin
+     Result := TSingleArea2D.NeInf;
+
+     for P in Ps_ do
+     begin
+          if P.X < Result.Min.X then Result.Min.X := P.X;
+          if P.Y < Result.Min.Y then Result.Min.Y := P.Y;
+
+          if Result.Max.X < P.X then Result.Max.X := P.X;
+          if Result.Max.Y < P.Y then Result.Max.Y := P.Y;
+     end;
+end;
+
+function BoundingBox( const Ps_:array of TDouble2D ) :TDoubleArea2D;
+var
+   P :TDouble2D;
+begin
+     Result := TDoubleArea2D.NeInf;
+
+     for P in Ps_ do
+     begin
+          if P.X < Result.Min.X then Result.Min.X := P.X;
+          if P.Y < Result.Min.Y then Result.Min.Y := P.Y;
+
+          if Result.Max.X < P.X then Result.Max.X := P.X;
+          if Result.Max.Y < P.Y then Result.Max.Y := P.Y;
+     end;
+end;
+
+//------------------------------------------------------------------------------
+
+function ShapeMatch( const Vs0_,Vs1_:array of TSingle2D ) :TSingleM2;
+var
+   I :Integer;
+   V0, V1 :TSingle2D;
+   M :TSingleM2;
+begin
+     Result := TSingleM2.Create( 0, 0,
+                                 0, 0 );
+
+     for I := 0 to High( Vs0_ ) do
+     begin
+          V0 := Vs0_[ I ];
+          V1 := Vs1_[ I ];
+
+          with M do
+          begin
+               _11 := V1.X * V0.X + V1.Y * V0.Y;  _12 := V1.X * V0.Y - V1.Y * V0.X;
+               _21 := V1.Y * V0.X - V1.X * V0.Y;  _22 := V1.X * V0.X + V1.Y * V0.Y;
+          end;
+
+          Result := Result + M;
+     end;
+
+     Result := Result / Roo2( Pow2( Result._11 ) + Pow2( Result._12 ) );
+end;
+
+function ShapeMatch( const Vs0_,Vs1_:array of TDouble2D ) :TDoubleM2;
+var
+   I :Integer;
+   V0, V1 :TDouble2D;
+   M :TDoubleM2;
+begin
+     Result := TDoubleM2.Create( 0, 0,
+                                 0, 0 );
+
+     for I := 0 to High( Vs0_ ) do
+     begin
+          V0 := Vs0_[ I ];
+          V1 := Vs1_[ I ];
+
+          with M do
+          begin
+               _11 := V1.X * V0.X + V1.Y * V0.Y;  _12 := V1.X * V0.Y - V1.Y * V0.X;
+               _21 := V1.Y * V0.X - V1.X * V0.Y;  _22 := V1.X * V0.X + V1.Y * V0.Y;
+          end;
+
+          Result := Result + M;
+     end;
+
+     Result := Result / Roo2( Pow2( Result._11 ) + Pow2( Result._12 ) );
 end;
 
 //############################################################################## □
