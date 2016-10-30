1b91ae3b5fbda2997f77124b2180a0f4938fc098
luxidea <contact@luxidea.net>
2016-10-30 09:18:14 +0900

    2D Shape Matching 法によるメッシュ膨張計算を追加。

--- Main.pas
+++ Main.pas
@@ -5,33 +5,40 @@ interface //####################################################################
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  System.Math.Vectors,
-  FMX.Objects, FMX.StdCtrls, FMX.Controls.Presentation,
+  System.Math.Vectors, System.Threading,
+  FMX.Objects, FMX.StdCtrls, FMX.Controls.Presentation, FMX.TabControl,
   FMX.Viewport3D, FMX.MaterialSources, FMX.Controls3D, FMX.Objects3D,
-  LUX, LUX.D2, LUX.Brep.Face.TriFlip.D2.Delaunay,
+  LUX, LUX.D2, LUX.Matrix.L2, LUX.Geometry.D2,
   YKX.Swellart;
 
 type
   TForm1 = class(TForm)
-    Viewport3D1: TViewport3D;
-      Camera1: TCamera;
-      Grid3D1: TGrid3D;
-      TextureMaterialSource1: TTextureMaterialSource;
+    TabControl1: TTabControl;
+      TabItem1: TTabItem;
+        Viewport3D1: TViewport3D;
+        Camera1: TCamera;
+        Grid3D1: TGrid3D;
+        TextureMaterialSource1: TTextureMaterialSource;
+      TabItem2: TTabItem;
+        Image1: TImage;
     Panel1: TPanel;
-      Image1: TImage;
       Button1: TButton;
       GroupBox1: TGroupBox;
+        Timer1: TTimer;
         Button2: TButton;
         Button3: TButton;
-    Timer1: TTimer;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
-    procedure Viewport3D1Paint(Sender: TObject; Canvas: TCanvas; const ARect: TRectF);
     procedure Viewport3D1Resize(Sender: TObject);
+    procedure Viewport3D1Paint(Sender: TObject; Canvas: TCanvas; const ARect: TRectF);
     procedure Viewport3D1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
     procedure Viewport3D1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
     procedure Viewport3D1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+    procedure Image1Paint(Sender: TObject; Canvas: TCanvas; const ARect: TRectF);
+    procedure Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+    procedure Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
+    procedure Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
@@ -43,15 +50,18 @@ type
     ///// メソッド
     function ScrToPos( const S_:TPointF ) :TSingle2D;                                                 //画面座標を内部座標へ
     function PosToScr( const P_:TSingle2D ) :TPointF;                                                 //画面座標を内部座標へ
-    procedure DrawCurve( const Canvas_:TCanvas; const Thickness_:Single; const Color_:TAlphaColor );  //手描カーブを描画
+    procedure DrawCurve( const Canvas_   :TCanvas;
+                         const Thickness_:Single;
+                         const Color_    :TAlphaColor;
+                         const Curve_    :TArray<TSingle2D> );                                        //カーブを描画
     procedure DrawPoins( const Canvas_:TCanvas; const Radius_:Single; const Color_:TAlphaColor );     //すべての格子点を描画
     procedure DrawFaces( const Canvas_:TCanvas; const Thickness_:Single; const Color_:TAlphaColor );  //すべての三角面を描画
-    procedure DrawVolos( const Canvas_:TCanvas; const Thickness_:Single; const Color_:TAlphaColor );  //ボロノイ図を描画
   public
     { public 宣言 }
-    _SwelCurve :TArray<TSingle2D>;  //手描カーブの配列
-    _SwelModel :TSwelModel;         //メッシュ
-    _SwelShape :TSwelShape;         //ポリゴン
+    _DougCurve :TArray<TSingle2D>;  //手描カーブの配列
+    _MoldCurve :TArray<TSingle2D>;  //手描カーブの配列
+    _DougModel :TDougModel;         //メッシュ
+    _DougShape :TDougShape;         //ポリゴン
   end;
 
 var
@@ -75,18 +85,21 @@ end;
 
 //------------------------------------------------------------------------------
 
-procedure TForm1.DrawCurve( const Canvas_:TCanvas; const Thickness_:Single; const Color_:TAlphaColor );  //手描カーブを描画
+procedure TForm1.DrawCurve( const Canvas_   :TCanvas;
+                            const Thickness_:Single;
+                            const Color_    :TAlphaColor;
+                            const Curve_    :TArray<TSingle2D> );  //カーブを描画
 var
    N, I :Integer;
    Vs :TPolygon;
 begin
-     if Assigned( _SwelCurve ) then  //手描カーブ配列が空でない場合
+     if Assigned( Curve_ ) then  //カーブ配列が空でない場合
      begin
-          N := Length( _SwelCurve );  //手描カーブ配列の要素数
+          N := Length( Curve_ );  //カーブ配列の要素数
 
           SetLength( Vs, N );  //多角形配列の要素数を設定
 
-          for I := 0 to N-1 do Vs[ I ] := PosToScr( _SwelCurve[ I ] );  //手描カーブの座標を画面座標へ変換
+          for I := 0 to N-1 do Vs[ I ] := PosToScr( Curve_[ I ] );  //カーブの座標を画面座標へ変換
 
           with Canvas_ do
           begin
@@ -120,9 +133,9 @@ begin
                Color := Color_;
           end;
 
-          for I := 0 to _SwelModel.PoinModel.ChildsN-1 do  //すべての格子点を走査
+          for I := 0 to _DougModel.PoinModel.ChildsN-1 do  //すべての格子点を走査
           begin
-               with PosToScr( _SwelModel.PoinModel.Childs[ I ].Pos ) do  //格子点
+               with PosToScr( _DougModel.PoinModel.Childs[ I ].Pos ) do  //格子点
                begin
                     FillEllipse( TRectF.Create( X-R, Y-R,
                                                 X+R, Y+R ), 1 );  //円を描画
@@ -136,10 +149,8 @@ end;
 procedure TForm1.DrawFaces( const Canvas_:TCanvas; const Thickness_:Single; const Color_:TAlphaColor );  //すべての三角面を描画
 var
    I :Integer;
-   Ps :TPolygon;
+   P1, P2, P3 :TPointF;
 begin
-     SetLength( Ps, 3 );
-
      with Canvas_ do
      begin
           with Stroke do  //ペン先の設定
@@ -150,58 +161,18 @@ begin
                Join      := TStrokeJoin.Round;
           end;
 
-          for I := 0 to _SwelModel.ChildsN-1 do
+          for I := 0 to _DougModel.ChildsN-1 do
           begin
-               with _SwelModel.Childs[ I ] do
+               with _DougModel.Childs[ I ] do
                begin
                     if Open = 0 then
                     begin
-                         Ps[ 0 ] := PosToScr( Poin[ 1 ].Pos );
-                         Ps[ 1 ] := PosToScr( Poin[ 2 ].Pos );
-                         Ps[ 2 ] := PosToScr( Poin[ 3 ].Pos );
+                         P1 := PosToScr( Poin[ 1 ].Pos0 );
+                         P2 := PosToScr( Poin[ 2 ].Pos0 );
+                         P3 := PosToScr( Poin[ 3 ].Pos0 );
 
-                         DrawPolygon( Ps, 1 );
+                         DrawPolygon( [ P1, P2, P3 ], 0.5 );
                     end;
-               end;
-          end;
-     end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TForm1.DrawVolos( const Canvas_:TCanvas; const Thickness_:Single; const Color_:TAlphaColor );  //ボロノイ図を描画
-var
-   I :Integer;
-   C, P1, P2, P3 :TSingle2D;
-begin
-     with Canvas_ do
-     begin
-          with Stroke do  //ペン先の設定
-          begin
-               Kind      := TBrushKind.Solid;
-               Color     := Color_;
-               Thickness := Thickness_ / _ViewS;
-               Join      := TStrokeJoin.Round;
-          end;
-
-          for I := 0 to _SwelModel.ChildsN-1 do
-          begin
-               with _SwelModel.Childs[ I ] do
-               begin
-                    C := Circle.Center;
-
-                    if Assigned( Face[ 1 ] ) then P1 := Ave( C, Face[ 1 ].CircumCircle.Center )
-                                             else P1 := Ave( Poin[ 2 ].Pos, Poin[ 3 ].Pos );
-
-                    if Assigned( Face[ 2 ] ) then P2 := Ave( C, Face[ 2 ].CircumCircle.Center )
-                                             else P2 := Ave( Poin[ 3 ].Pos, Poin[ 1 ].Pos );
-
-                    if Assigned( Face[ 3 ] ) then P3 := Ave( C, Face[ 3 ].CircumCircle.Center )
-                                             else P3 := Ave( Poin[ 1 ].Pos, Poin[ 2 ].Pos );
-
-                    DrawLine( PosToScr( C ), PosToScr( P1 ), 1 );
-                    DrawLine( PosToScr( C ), PosToScr( P2 ), 1 );
-                    DrawLine( PosToScr( C ), PosToScr( P3 ), 1 );
                end;
           end;
      end;
@@ -213,23 +184,23 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);  //アプリを起動した場合
 begin
-     Image1.Bitmap.LoadFromFile( '..\..\_DATA\Texture.png' );  //テクスチャを読込み
+     Image1.Bitmap.LoadFromFile( '..\..\_DATA\Dough.jpg' );  //テクスチャを読込み
 
      TextureMaterialSource1.Texture.Assign( Image1.Bitmap );  //テクスチャをシェーダへコピー
 
-     _SwelModel := TSwelModel.Create;  //メッシュを生成
+     _DougModel := TDougModel.Create;  //メッシュを生成
 
-     with _SwelModel do
+     with _DougModel do
      begin
           Radius := 2;  //三角面の最小外接円半径
      end;
 
-     _SwelShape := TSwelShape.Create( Self );  //ポリゴンを生成
+     _DougShape := TDougShape.Create( Self );  //ポリゴンを生成
 
-     with _SwelShape do
+     with _DougShape do
      begin
           Parent   := Viewport3D1;             //ビューポートへ登録
-          Model    := _SwelModel;              //メッシュモデルを登録
+          Model    := _DougModel;              //メッシュモデルを登録
           Material := TextureMaterialSource1;  //シェーダを登録
      end;
 
@@ -238,46 +209,10 @@ end;
 
 procedure TForm1.FormDestroy(Sender: TObject);  //アプリを終了した場合
 begin
-     _SwelShape.Free;  //ポリゴンを廃棄
-     _SwelModel.Free;  //メッシュを廃棄
+     _DougModel.Free;  //メッシュを廃棄
 end;
 
 ////////////////////////////////////////////////////////////////////////////////
-
-procedure TForm1.Timer1Timer(Sender: TObject);
-var
-   I :Integer;
-begin
-     for I := 0 to _SwelModel.ChildsN-1 do  //すべての三角面を走査
-     begin
-          with _SwelModel.Childs[ I ] do  //三角面
-          begin
-               {なんらかの処理}
-          end;
-     end;
-
-     for I := 0 to _SwelModel.PoinModel.ChildsN-1 do  //すべての格子点を走査
-     begin
-          with TSwelPoin( _SwelModel.PoinModel.Childs[ I ] ) do  //格子点
-          begin
-               {なんらかの処理}
-          end;
-     end;
-
-     _SwelShape.UpdateGeometry;  //ポリゴンを更新
-
-     Viewport3D1.Repaint;  //ピューを更新
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TForm1.Viewport3D1Paint(Sender: TObject; Canvas: TCanvas; const ARect: TRectF);  //ビューポートが描画される場合
-begin
-     DrawCurve( Canvas, 0.4, TAlphaCOlors.Red   );  //手描カーブを描画
-   //DrawFaces( Canvas, 0.1, TAlphaCOlors.Black );  ////すべての三角面を描画
-     DrawVolos( Canvas, 0.2, TAlphaCOlors.White );  //ボロノイ図を描画
-     DrawPoins( Canvas, 0.2, TAlphaCOlors.Black );  //すべての格子点を描画
-end;
 
 procedure TForm1.Viewport3D1Resize(Sender: TObject);  //ビューポートがリサイズされる場合
 begin
@@ -287,49 +222,51 @@ begin
                                Viewport3D1.Height / 2 );  //ビューポートの中心座標
 end;
 
+procedure TForm1.Viewport3D1Paint(Sender: TObject; Canvas: TCanvas; const ARect: TRectF);  //ビューポートが描画される時
+begin
+     DrawCurve( Canvas, 0.4, TAlphaCOlors.Blue , _MoldCurve );  //手描カーブを描画
+     DrawPoins( Canvas, 0.2, TAlphaCOlors.White             );  //すべての格子点を描画
+end;
+
 //------------------------------------------------------------------------------
 
-procedure TForm1.Viewport3D1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);  //マウスを押した場合
+procedure TForm1.Viewport3D1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);  //マウスを押した時
 begin
      if Button2.Enabled then  //最適化が実行されていない場合
      begin
           _MouseState := Shift;  //マウスボタンの状態を保存
 
-          _SwelCurve := [ ScrToPos( TPointF.Create( X, Y ) ) ];  //描き始めの座標を追加
+          _MoldCurve := [ ScrToPos( TPointF.Create( X, Y ) ) ];  //描き始めの座標を追加
 
-          _SwelModel.DeleteChilds;  //メッシュをクリア
+          _DougModel.DeleteChilds;  //メッシュをクリア
 
-          _SwelShape.MakeGeometry;  //ポリゴンを再構築
+          _DougShape.MakeGeometry;  //ポリゴンを再構築
      end;
 end;
 
-procedure TForm1.Viewport3D1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);  //マウスを動かした場合
+procedure TForm1.Viewport3D1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);  //マウスを動かした時
 begin
      if ssLeft in _MouseState then  //左クリックの場合
      begin
-          _SwelCurve := _SwelCurve + [ ScrToPos( TPointF.Create( X, Y ) ) ];  //移動した座標を追加
+          _MoldCurve := _MoldCurve + [ ScrToPos( TPointF.Create( X, Y ) ) ];  //移動した座標を追加
 
           Viewport3D1.Repaint;  //ビューを再描画
      end;
 end;
 
-procedure TForm1.Viewport3D1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);  //マウスを離した場合
+procedure TForm1.Viewport3D1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);  //マウスを離した時
 begin
      if ssLeft in _MouseState then
      begin
-          _SwelCurve := _SwelCurve + [ ScrToPos( TPointF.Create( X, Y ) ) ];  //描き終わりの座標を追加
+          _MoldCurve := _MoldCurve + [ ScrToPos( TPointF.Create( X, Y ) ) ];  //描き終わりの座標を追加
 
-          with _SwelModel do
+          if Length( _MoldCurve ) >= 3 then
           begin
-               MakeMesh( _SwelCurve );  //メッシュを生成
-               FairMesh;                //不要な格子点と三角面を削除
-          end;
-
-          _SwelShape.MakeGeometry;  //ポリゴンを再構築
+               {型輪郭の処理}
+          end
+          else _MoldCurve := [];
 
           Viewport3D1.Repaint;  //ビューを再描画
-
-          _SwelCurve := [];
 
           _MouseState := [];
      end;
@@ -337,42 +274,181 @@ end;
 
 //------------------------------------------------------------------------------
 
-procedure TForm1.Button1Click(Sender: TObject);  //"筋繊維生成"ボタンを押した場合
+procedure TForm1.Image1Paint(Sender: TObject; Canvas: TCanvas; const ARect: TRectF);
+begin
+     DrawCurve( Canvas, 0.4, TAlphaCOlors.Red  ,_DougCurve );  //手描カーブを描画
+     DrawFaces( Canvas, 0.1, TAlphaCOlors.White            );  //すべての三角面を描画
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+begin
+     if Button2.Enabled then  //最適化が実行されていない場合
+     begin
+          _MouseState := Shift;  //マウスボタンの状態を保存
+
+          _DougCurve := [ ScrToPos( TPointF.Create( X, Y ) ) ];  //描き始めの座標を追加
+
+          _DougModel.DeleteChilds;  //メッシュをクリア
+
+          _DougShape.MakeGeometry;  //ポリゴンを再構築
+     end;
+end;
+
+procedure TForm1.Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
+begin
+     if ssLeft in _MouseState then  //左クリックの場合
+     begin
+          _DougCurve := _DougCurve + [ ScrToPos( TPointF.Create( X, Y ) ) ];  //移動した座標を追加
+
+          Image1.Repaint;  //ビューを再描画
+     end;
+end;
+
+procedure TForm1.Image1MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+begin
+     if ssLeft in _MouseState then
+     begin
+          _DougCurve := _DougCurve + [ ScrToPos( TPointF.Create( X, Y ) ) ];  //描き終わりの座標を追加
+
+          if Length( _DougCurve ) >= 3 then
+          begin
+               with _DougModel do
+               begin
+                    MakeMesh( _DougCurve );  //メッシュを生成
+                    FairMesh;                //不要な格子点と三角面を削除
+               end;
+
+               _DougShape.MakeGeometry;  //ポリゴンを再構築
+
+               Button1Click( Sender );  //「初期化」ボタンを実行
+          end;
+
+          _DougCurve := [];
+
+          Image1.Repaint;
+
+          _MouseState := [];
+     end;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.Button1Click(Sender: TObject);  //「初期化」ボタンを押した時
 var
    I :Integer;
+   P, T :TPointF;
 begin
-     for I := 0 to _SwelModel.ChildsN-1 do  //すべての三角面を走査
+     for I := 0 to _DougModel.PoinModel.ChildsN-1 do  //すべての三角面を走査
      begin
-          with _SwelModel.Childs[ I ] do  //三角面
+          with _DougModel.PoinModel.Childs[ I ] do  //三角面
           begin
-               {なんらかの処理}
+               Pos0 := Pos;  //初期の座標を保存
+
+               P := PosToScr( Pos0 );
+
+               T.X := P.X / Image1.Width ;
+               T.Y := P.Y / Image1.Height;
+
+               Tex := T;
           end;
      end;
 
-     for I := 0 to _SwelModel.ChildsN-1 do  //すべての三角点を走査
-     begin
-          with _SwelModel.Childs[ I ] do  //三角点
-          begin
-               {なんらかの処理}
-          end;
-     end;
-
-     _SwelShape.UpdateGeometry;  //ポリゴンを更新
+     _DougShape.UpdateGeometry;  //ポリゴンを更新
 
      Viewport3D1.Repaint;  //ビューを再描画
 end;
 
-procedure TForm1.Button2Click(Sender: TObject);  //実行ボタンを押した場合
+//------------------------------------------------------------------------------
+
+procedure TForm1.Timer1Timer(Sender: TObject);  //最適化の１フレーム
+var
+   N, I, K :Integer;
+   G0, G1 :TSingle2D;
+   P0, P1, P2, V0, V1 :array [1..3] of TSingle2D;
+   M :TSingleM2;
+begin
+     for N := 1 to 10 do
+     begin
+          TParallel.For( 0, _DougModel.PoinModel.ChildsN-1, procedure( I:Integer )  //すべての格子点を走査(並列化)
+          begin
+               with _DougModel.PoinModel.Childs[ I ] do  //格子点
+               begin
+                    Force := TSingle2D.Create( 0, 0 );  //格子点の力を初期化
+               end;
+          end,
+          _ThreadPool );
+
+          for I := 0 to _DougModel.ChildsN-1 do  //すべての三角面を走査
+          begin
+               with _DougModel.Childs[ I ] do  //三角面
+               begin
+                    G0 := Barycenter0;  //初期の重心
+                    G1 := Barycenter ;  //現在の重心
+
+                    for K := 1 to 3 do
+                    begin
+                         P0[K] := Poin[K].Pos0;  //初期の座標
+                         P1[K] := Poin[K].Pos ;  //現在の座標
+
+                         V0[K] := P0[K] - G0;    //初期のベクトル
+                         V1[K] := P1[K] - G1;    //現在のベクトル
+
+                         V0[K] := V0[K] * 2;     //理想とする形
+                    end;
+
+                    M := ShapeMatch( V0, V1 );  //最適な回転行列
+
+                    for K := 1 to 3 do
+                    begin
+                         P2[K] := M * V0[K] + G1;  //理想の座標
+
+                         ///// 位置の制約
+
+                         if P2[K].X < -30 then P2[K].X := -30
+                                          else
+                         if +30 < P2[K].X then P2[K].X := +30;
+
+                         if P2[K].Y < -30 then P2[K].Y := -30
+                                          else
+                         if +30 < P2[K].Y then P2[K].Y := +30;
+
+                         /////
+
+                         with Poin[K] do Force := Force + ( P2[K] - P1[K] );  //力を格子点へ加算
+                    end;
+               end;
+          end;
+
+          TParallel.For( 0, _DougModel.PoinModel.ChildsN-1, procedure( I:Integer )  //すべての格子点を走査(並列化)
+          begin
+               with _DougModel.PoinModel.Childs[ I ] do  //格子点
+               begin
+                    Pos := Pos + 0.1 * Force;  //格子点を移動
+               end;
+          end,
+          _ThreadPool );
+     end;
+
+     _DougShape.UpdateGeometry;  //ポリゴンの座標を更新
+
+     Viewport3D1.Repaint;  //ピューポートを更新
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);  //「実行」ボタンを押した時
 begin
      Button2.Enabled := False;
      Button3.Enabled := True ;
 
-     Timer1.Enabled := True ;  //シミュレーション開始
+     TabControl1.TabIndex := 0;  //タブを１ページ目に切り替え
+
+     Timer1.Enabled := True ;  //最適化開始
 end;
 
-procedure TForm1.Button3Click(Sender: TObject);  //停止ボタンを押した場合
+procedure TForm1.Button3Click(Sender: TObject);  //「停止」ボタンを押した時
 begin
-     Timer1.Enabled := False;  //シミュレーション停止
+     Timer1.Enabled := False;  //最適化停止
 
      Button2.Enabled := True ;
      Button3.Enabled := False;
