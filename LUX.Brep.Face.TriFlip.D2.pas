80852d8657f587bcf5f4a7ca93f3a1ac5504ff5a
luxidea <contact@luxidea.net>
2016-11-03 16:33:04 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX.Brep/' changes from d9868d9..c49ced5
    c49ced5 重心座標を算出する TTriFace2D.BaryPos メソッドを追加。
    6a45529 Merge commit 'a7a3fb6d94336e662ad4b5d582a7d81d98a1d619' into nakayama
    a7a3fb6 Squashed '_LIBRARY/LUXOPHIA/LUX.Geometry/' changes from b581713..65bea09
    bf05e43 Merge commit 'b47465be8dcf611bab38dde74bd5802d58660adc' into nakayama
    642a232 Merge commit '991e47c88633ac4bf92a3594bdea9cb3e1259f6b' into nakayama
    991e47c Squashed '_LIBRARY/LUXOPHIA/LUX.Matrix/' changes from 275c015..d26eef5
    35ac656 InsideEdges メソッドでの内外判定処理を LUX.Geometry.D2.InsideLoop 関数へ外部化。
    1fa92a2 Merge commit '529fa85eb9a2d9fd111eb567f29a3f652b52911d' into nakayama
    529fa85 Squashed '_LIBRARY/LUXOPHIA/LUX.Matrix/' changes from 6aec6dc..275c015
    a22baae no message
    96fc287 Merge commit '8084db6ec13b327665719533c811699cd7c67eba' into nakayama
    ad9f084 no message
    09e8c7b Squashed '_LIBRARY/LUXOPHIA/LUX.Geometry/' changes from 7b1cc82..b581713
    a6a861b Add '_LIBRARY/LUXOPHIA/LUX.Map/' from commit 'fc0e82d239db7d5b29adc9ed31e8f844a097f616'
    d1421b0 Merge commit '47b6e3771064230004ac27485cf69235cd3d6524' into nakayama
    e998a3d Merge commit 'dba195711718b0e384b8547bef8504438d8a4405' into nakayama
    dba1957 Squashed '_LIBRARY/LUXOPHIA/LUX.Geometry/' changes from 2e166e9..619595b
    0868d9f Merge commit 'ac109d7d5be2ab3888a86b8d382a9d926ed908af' into nakayama
    ac109d7 Squashed '_LIBRARY/LUXOPHIA/LUX.Matrix/' changes from 1a16aec..275c015
    e16bb72 Face を削除した際、隣接する Face の結合ポインタに nil を代入。
    fc0e82d no message
    4ebd008 Merge commit '6c2a1c4d828b6d93095883ea47f076f518d15a18'
    d8f0317 Merge commit '3fb22f0b4a21e2e7f813c803a7e23cdf400ea123'
    b121c65 Bric/GridArray を追加。
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX.Brep
    git-subtree-split: c49ced5857be6e72f8feeb08d251df24c6cc81e2

--- LUX.Brep.Face.TriFlip.D2.pas
+++ LUX.Brep.Face.TriFlip.D2.pas
@@ -3,7 +3,7 @@
 interface //#################################################################### ■
 
 uses System.RegularExpressions,
-     LUX, LUX.D2, LUX.Geometry.D2, LUX.Brep.Face.TriFlip;
+     LUX, LUX.D2, LUX.Matrix.L3, LUX.Geometry.D2, LUX.Brep.Face.TriFlip;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
@@ -41,6 +41,9 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        property Norv[ const I_:Byte ] :TSingle2D     read GetNorv        ;
        property CircumCircle          :TSingleCircle read GetCircumCircle;
        property Barycenter            :TSingle2D     read GetBarycenter  ;
+       ///// メソッド
+       function BaryPos( const P_:TSingle2D ) :TSingleV3;
+       function IsInside( const P_:TSingle2D ) :Boolean;
      end;
 
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TTriFaceModel2D<_TPoin_>
@@ -57,6 +60,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        ///// メソッド
        procedure JoinEdges;
        procedure LoadFromFile( const FileName_:String );
+       function FindHit( const P_:TSingle2D; out B_:TSingleV3 ) :_TFace_;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -110,6 +114,35 @@ begin
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TTriFace2D.BaryPos( const P_:TSingle2D ) :TSingleV3;
+var
+   P1, P2, P3 :TSingle2D;
+   M :TSingleM3;
+begin
+     P1 := Poin[1].Pos;
+     P2 := Poin[2].Pos;
+     P3 := Poin[3].Pos;
+
+     with M do
+     begin
+          _11 := P1.X;  _12 := P2.X;  _13 := P3.X;
+          _21 := P1.Y;  _22 := P2.Y;  _23 := P3.Y;
+          _31 :=    1;  _32 :=    1;  _33 :=    1;
+     end;
+
+     Result := M.Inverse * TSingleV3.Create( P_.X, P_.Y, 1 );
+end;
+
+function TTriFace2D.IsInside( const P_:TSingle2D ) :Boolean;
+begin
+     with BaryPos( P_ ) do
+     begin
+          Result := ( _1 > 0 ) and ( _2 > 0 ) and ( _3 > 0 );
+     end;
+end;
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TTriFaceModel2D<_TPoin_>
 
@@ -232,6 +265,36 @@ begin
      end;
 
      JoinEdges;
+end;
+
+//------------------------------------------------------------------------------
+
+function TTriFaceModel2D<_TPoin_,_TFace_>.FindHit( const P_:TSingle2D; out B_:TSingleV3 ) :_TFace_;
+var
+   I :Integer;
+   F :_TFace_;
+   B :TSingleV3;
+begin
+     for I := 0 to ChildsN-1 do
+     begin
+          F := Childs[ I ];
+
+          B := F.BaryPos( P_ );
+
+          with B do
+          begin
+               if ( _1 > 0 ) and ( _2 > 0 ) and ( _3 > 0 ) then
+               begin
+                    Result := F;
+
+                    B_ := B;
+
+                    Exit;
+               end;
+          end;
+     end;
+
+     Result := nil;
 end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
