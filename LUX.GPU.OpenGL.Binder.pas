a09ba1e4779d48b1334fed6de64cd2d501997cf6
luxidea <contact@luxidea.net>
2017-05-19 08:55:59 +0900

    TGLPlugerV / TGLPlugerU クラスを追加。

--- LUX.GPU.OpenGL.Binder.pas
+++ LUX.GPU.OpenGL.Binder.pas
@@ -2,29 +2,85 @@
 
 interface //#################################################################### ■
 
-uses Winapi.OpenGL, Winapi.OpenGLext,
-     LUX;
+uses System.Generics.Collections,
+     Winapi.OpenGL, Winapi.OpenGLext,
+     LUX, LUX.GPU.OpenGL, LUX.GPU.OpenGL.Buffer.Vert, LUX.GPU.OpenGL.Buffer.Unif;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLVerPlug
+
+     TGLVerPlug = record
+     private
+     public
+       BuffV :IGLBufferV;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLUniPlug
+
+     TGLUniPlug = record
+     private
+     public
+       BuffU :IGLBufferU;
+       Start :Integer;
+       Count :Integer;
+     end;
+
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLBinder
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLVerPlugs
 
-     TGLBinder = class
+     TGLVerPlugs = class( TDictionary<GLuint,TGLVerPlug> )
      private
      protected
-       _ID :GLuint;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLUniPlugs
+
+     TGLUniPlugs = class( TDictionary<GLuint,TGLUniPlug> )
+     private
+     protected
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPlugerV
+
+     TGLPlugerV = class( TGLObject )
+     private
+     protected
+       _Plugs :TGLVerPlugs;
      public
        constructor Create;
        destructor Destroy; override;
        ///// プロパティ
-       property ID :GLuint read _ID;
+       property Plugs :TGLVerPlugs read _Plugs;
        ///// メソッド
+       procedure Add( const BindP_:GLuint; const BuffV_:IGLBufferV );
        procedure Use;
-       class procedure Unuse;
+       procedure Unuse;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPlugerU
+
+     TGLPlugerU = class( TGLObject )
+     private
+     protected
+       _Plugs :TGLUniPlugs;
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Plugs :TGLUniPlugs read _Plugs;
+       ///// メソッド
+       procedure Add( const BindP_:GLuint;
+                      const BuffU_:IGLBufferU;
+                      const Start_:Integer = -1;
+                      const Count_:Integer = 1 );
+       procedure Use;
+       procedure Unuse;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -37,9 +93,21 @@ implementation //###############################################################
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLVerPlug
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLUniPlug
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLBinder
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLVerPlugs
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
@@ -47,30 +115,137 @@ implementation //###############################################################
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
-constructor TGLBinder.Create;
-begin
-     inherited Create;
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLUniPlugs
 
-     glGenVertexArrays( 1, @_ID );
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPlugerV
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLPlugerV.Create;
+begin
+     inherited;
+
+     _Plugs := TGLVerPlugs.Create;
 end;
 
-destructor TGLBinder.Destroy;
+destructor TGLPlugerV.Destroy;
 begin
-     glDeleteVertexArrays( 1, @_ID );
+     _Plugs.DisposeOf;
 
      inherited;
 end;
 
 /////////////////////////////////////////////////////////////////////// メソッド
 
-procedure TGLBinder.Use;
+procedure TGLPlugerV.Add( const BindP_:GLuint; const BuffV_:IGLBufferV );
+var
+   B :TGLVerPlug;
 begin
-     glBindVertexArray( _ID );
+     with B do
+     begin
+          BuffV := BuffV_;
+     end;
+
+     _Plugs.AddOrSetValue( BindP_, B );
 end;
 
-class procedure TGLBinder.Unuse;
+//------------------------------------------------------------------------------
+
+procedure TGLPlugerV.Use;
+var
+   P :TPair<GLuint,TGLVerPlug>;
 begin
-     glBindVertexArray( 0 );
+     for P in _Plugs do
+     begin
+          with P do Value.BuffV.Use( Key );
+     end;
+end;
+
+procedure TGLPlugerV.Unuse;
+var
+   P :TPair<GLuint,TGLVerPlug>;
+begin
+     for P in _Plugs do
+     begin
+          with P do Value.BuffV.Unuse( Key );
+     end;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPlugerU
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLPlugerU.Create;
+begin
+     inherited;
+
+     _Plugs := TGLUniPlugs.Create;
+end;
+
+destructor TGLPlugerU.Destroy;
+begin
+     _Plugs.DisposeOf;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLPlugerU.Add( const BindP_:GLuint;
+                          const BuffU_:IGLBufferU;
+                          const Start_:Integer = -1;
+                          const Count_:Integer = 1 );
+var
+   B :TGLUniPlug;
+begin
+     with B do
+     begin
+          BuffU := BuffU_;
+          Start := Start_;
+          Count := Count_;
+     end;
+
+     _Plugs.AddOrSetValue( BindP_, B );
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TGLPlugerU.Use;
+var
+   P :TPair<GLuint,TGLUniPlug>;
+begin
+     for P in _Plugs do
+     begin
+          with P, Value do
+          begin
+               if Start = -1 then BuffU.Use( Key )
+                             else BuffU.Use( Key, Start, Count );
+          end;
+     end;
+end;
+
+procedure TGLPlugerU.Unuse;
+var
+   P :TPair<GLuint,TGLUniPlug>;
+begin
+     for P in _Plugs do
+     begin
+          with P do Value.BuffU.Unuse( Key );
+     end;
 end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
