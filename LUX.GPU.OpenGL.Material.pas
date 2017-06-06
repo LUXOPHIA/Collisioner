55872f9acb25d770d42d43e2e09799438cb0288d
luxidea <contact@luxidea.net>
2017-06-07 06:39:47 +0900

    TGLMaterial クラスを追加。

--- LUX.GPU.OpenGL.Material.pas
+++ LUX.GPU.OpenGL.Material.pas
@@ -2,17 +2,37 @@
 
 interface //#################################################################### ■
 
-uses LUX, LUX.GPU.OpenGL;
+uses Winapi.OpenGL, Winapi.OpenGLext,
+     LUX,
+     LUX.GPU.OpenGL,
+     LUX.GPU.OpenGL.Shader,
+     LUX.GPU.OpenGL.Engine;
 
-//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
-
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMaterial
+
+     TGLMaterial = class
+     private
+     protected
+       _ShaderV :TGLShaderV;
+       _ShaderF :TGLShaderF;
+       _Engine  :TGLEngine;
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// プロパティ
+       property ShaderV :TGLShaderV       read _ShaderV;
+       property ShaderF :TGLShaderF       read _ShaderF;
+       property Engine  :TGLEngine        read _Engine ;
+       ///// メソッド
+       procedure Use; virtual;
+       procedure Unuse; virtual;
+     end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
 
@@ -24,21 +44,93 @@ implementation //###############################################################
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMaterial
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLMaterial.Create;
+begin
+     inherited;
+
+     _ShaderV := TGLShaderV.Create;
+     _ShaderF := TGLShaderF.Create;
+     _Engine  := TGLEngine .Create;
+
+     with _ShaderV do
+     begin
+          OnCompiled := procedure
+          begin
+               _Engine.Link;
+          end;
+     end;
+
+     with _ShaderF do
+     begin
+          OnCompiled := procedure
+          begin
+               _Engine.Link;
+          end;
+     end;
+
+     with _Engine do
+     begin
+          with Shaders do
+          begin
+               Add( _ShaderV{Shad} );
+               Add( _ShaderF{Shad} );
+          end;
+
+          with VerBufs do
+          begin
+               Add( 0{BinP}, '_Vertex_Pos'{Name}, 3{EleN}, GL_FLOAT{EleT} );
+               Add( 1{BinP}, '_Vertex_Nor'{Name}, 3{EleN}, GL_FLOAT{EleT} );
+               Add( 2{BinP}, '_Vertex_Tex'{Name}, 2{EleN}, GL_FLOAT{EleT} );
+          end;
+
+          with UniBufs do
+          begin
+               Add( 0{BinP}, 'TCamera'{Name} );
+               Add( 1{BinP}, 'TGeomet'{Name} );
+          end;
+
+          with Imagers do
+          begin
+               Add( 0{BinP}, '_Imager'{Name} );
+          end;
+
+          with Framers do
+          begin
+               Add( 0{BinP}, '_Frag_Col'{Name} );
+          end;
+     end;
+end;
+
+destructor TGLMaterial.Destroy;
+begin
+     _ShaderV.DisposeOf;
+     _ShaderF.DisposeOf;
+     _Engine .DisposeOf;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLMaterial.Use;
+begin
+     _Engine.Use;
+end;
+
+procedure TGLMaterial.Unuse;
+begin
+     _Engine.Unuse;
+end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
