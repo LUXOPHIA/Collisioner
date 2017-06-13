2af5d5cfb0a26ae99124263f3f3f301ca25a61a3
luxidea <contact@luxidea.net>
2017-06-14 04:00:28 +0900

    LUX.GPU.OpenGL.Scener / Camera ユニットを追加。

---
+++ LUX.GPU.OpenGL.Camera.pas
@@ -0,0 +1,124 @@
+﻿unit LUX.GPU.OpenGL.Camera;
+
+interface //#################################################################### ■
+
+uses Winapi.OpenGL, Winapi.OpenGLext,
+     LUX, LUX.D2, LUX.D3, LUX.M4, LUX.Tree,
+     LUX.GPU.OpenGL,
+     LUX.GPU.OpenGL.Buffer,
+     LUX.GPU.OpenGL.Buffer.Unif,
+     LUX.GPU.OpenGL.Buffer.Vert,
+     LUX.GPU.OpenGL.Buffer.Elem,
+     LUX.GPU.OpenGL.Scener,
+     LUX.GPU.OpenGL.Matery;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLCamera
+
+     TGLCamera = class( TGLNode )
+     private
+       ///// メソッド
+       procedure RegBuf;
+     protected
+       _Proj :TSingleM4;
+       ///// アクセス
+       procedure SetMove( const Move_:TSingleM4 ); override;
+       procedure SetProj( const Proj_:TSingleM4 ); virtual;
+     public
+       _No :Integer;
+       constructor Create; override;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Proj :TSingleM4 read _Proj write SetProj;
+       ///// メソッド
+       procedure Render;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses System.SysUtils, System.Classes;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLCamera
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLCamera.RegBuf;
+var
+   C :TCameraDat;
+begin
+     C.Proj := _Proj;
+     C.Move := _Move;
+
+     ( RootNode as TGLScener ).CameraUs[ _No ] := C;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+procedure TGLCamera.SetMove( const Move_:TSingleM4 );
+begin
+     inherited;
+
+     _Move := Move_;  RegBuf;
+end;
+
+procedure TGLCamera.SetProj( const Proj_:TSingleM4 );
+begin
+     _Proj := Proj_;  RegBuf;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLCamera.Create;
+begin
+     inherited;
+
+end;
+
+destructor TGLCamera.Destroy;
+begin
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLCamera.Render;
+begin
+     with ( RootNode as TGLScener ) do
+     begin
+          CameraUs.Use( 0{BinP}, _No{Offs} );
+
+          Render;
+
+          CameraUs.Unuse( 0{BinP} );
+     end;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
---
+++ LUX.GPU.OpenGL.Scener.pas
@@ -0,0 +1,170 @@
+﻿unit LUX.GPU.OpenGL.Scener;
+
+interface //#################################################################### ■
+
+uses Winapi.OpenGL, Winapi.OpenGLext,
+     LUX, LUX.D2, LUX.D3, LUX.M4, LUX.Tree,
+     LUX.GPU.OpenGL,
+     LUX.GPU.OpenGL.Buffer,
+     LUX.GPU.OpenGL.Buffer.Unif,
+     LUX.GPU.OpenGL.Buffer.Vert,
+     LUX.GPU.OpenGL.Buffer.Elem,
+     LUX.GPU.OpenGL.Matery;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TGLScener = class;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     TCameraDat = record
+     private
+     public
+       Proj :TSingleM4;
+       Move :TSingleM4;
+     end;
+
+     TShaperDat = record
+     private
+     public
+       Move :TSingleM4;
+     end;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLNode
+
+     TGLNode = class( TTreeNode<TGLNode> )
+     private
+     protected
+       _Move :TSingleM4;
+       ///// アクセス
+       procedure SetMove( const Move_:TSingleM4 ); virtual;
+     public
+       constructor Create; override;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Move :TSingleM4 read _Move write SetMove;
+       ///// メソッド
+       procedure Draw; virtual;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLWorld
+
+     TGLScener = class( TGLNode )
+     private
+     protected
+       _CameraUs :TGLBufferU<TCameraDat>;
+       _GeometUs :TGLBufferU<TShaperDat>;
+     public
+       constructor Create; override;
+       destructor Destroy; override;
+       ///// プロパティ
+       property CameraUs :TGLBufferU<TCameraDat> read _CameraUs;
+       property GeometUs :TGLBufferU<TShaperDat> read _GeometUs;
+       ///// メソッド
+       procedure Compile;
+       procedure Render;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses System.SysUtils, System.Classes;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLNode
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+procedure TGLNode.SetMove( const Move_:TSingleM4 );
+var
+   D :TShaperDat;
+begin
+     _Move := Move_;
+
+     D.Move := _Move;
+
+     ( RootNode as TGLScener )._GeometUs[ Order ] := D;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLNode.Create;
+begin
+     inherited;
+
+end;
+
+destructor TGLNode.Destroy;
+begin
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLNode.Draw;
+begin
+
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLWorld
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLScener.Create;
+begin
+     inherited;
+
+     _CameraUs := TGLBufferU<TCameraDat>.Create( GL_DYNAMIC_DRAW );
+     _GeometUs := TGLBufferU<TShaperDat>.Create( GL_DYNAMIC_DRAW );
+end;
+
+destructor TGLScener.Destroy;
+begin
+     _CameraUs.Free;
+     _GeometUs.Free;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLScener.Compile;
+begin
+
+end;
+
+procedure TGLScener.Render;
+var
+   I :Integer;
+begin
+     for I := 0 to ChildsN-1 do Childs[ I ].Draw;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
