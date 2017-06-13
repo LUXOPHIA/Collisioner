96eb4cbeaec7e5452b58a0adf14a14b24f4176ef
luxidea <contact@luxidea.net>
2017-06-14 06:42:12 +0900

    TGLNode クラスに IGLNode インタフェースを導入。 TGLNode.NodI プロバティを追加。 TGLScener クラスに IGLScener インタフェースを導入。 TGLScener.Cameras / Nodes プロパティを追加。 IGLCamera / IGLShaper インタフェースを追加。

--- LUX.GPU.OpenGL.Scener.pas
+++ LUX.GPU.OpenGL.Scener.pas
@@ -2,7 +2,8 @@
 
 interface //#################################################################### ■
 
-uses Winapi.OpenGL, Winapi.OpenGLext,
+uses System.Generics.Collections,
+     Winapi.OpenGL, Winapi.OpenGLext,
      LUX, LUX.D2, LUX.D3, LUX.M4, LUX.Tree,
      LUX.GPU.OpenGL,
      LUX.GPU.OpenGL.Buffer,
@@ -24,7 +25,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        Move :TSingleM4;
      end;
 
-     TShaperDat = record
+     TNodeDat = record
      private
      public
        Move :TSingleM4;
@@ -34,39 +35,85 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLNode
 
-     TGLNode = class( TTreeNode<TGLNode> )
+     IGLNode = interface
+     ['{049DB60C-9F5D-45B9-89D8-E4AC58C807E3}']
+     {protected}
+       ///// アクセス
+       function GetScener :TGLScener;
+       function GetMove :TSingleM4;
+       procedure SetMove( const Move_:TSingleM4 );
+     {public}
+       ///// プロパティ
+       property Scener :TGLScener read GetScener              ;
+       property Move   :TSingleM4 read GetMove   write SetMove;
+       ///// メソッド
+       procedure RegBuf;
+       procedure Draw;
+     end;
+
+     TGLNode = class( TTreeNode<TGLNode>, IGLNode )
      private
      protected
        _Move :TSingleM4;
        ///// アクセス
        function GetScener :TGLScener;
+       function GetNodI :Integer;
+       function GetMove :TSingleM4;
        procedure SetMove( const Move_:TSingleM4 ); virtual;
      public
        constructor Create; override;
        destructor Destroy; override;
        ///// プロパティ
        property Scener :TGLScener read GetScener              ;
-       property Move   :TSingleM4 read   _Move   write SetMove;
+       property NodI   :Integer   read GetNodI                ;
+       property Move   :TSingleM4 read GetMove   write SetMove;
        ///// メソッド
+       procedure RegBuf; virtual;
        procedure Draw; virtual;
      end;
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLWorld
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLScener
 
-     TGLScener = class( TGLNode )
+     IGLCamera = interface( IGLNode )
+     ['{648646AC-975D-464E-BD83-C39EA3EB4E1E}']
+     {protected}
+     {public}
+       ///// メソッド
+       procedure RegBuf;
+     end;
+
+     IGLShaper = interface( IGLNode )
+     ['{8045CCEA-8FC4-4D0A-A6CE-A97FF6972A7F}']
+     {protected}
+     {public}
+     end;
+
+     IGLScener = interface( IGLNode )
+     ['{600C6A00-B748-4A1B-A841-A7135257ABCA}']
+     {protected}
+     {public}
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TGLScener = class( TGLNode, IGLScener )
      private
+       _Cameras :TDictionary<IGLCamera,Integer>;
+       _Nodes   :TDictionary<IGLNode,Integer>;
      protected
        _CameraUs :TGLBufferU<TCameraDat>;
-       _ShaperUs :TGLBufferU<TShaperDat>;
+       _NodeUs   :TGLBufferU<TNodeDat>;
+       ///// メソッド
+       procedure OnInsertChild( const Child_:TTreeNode ); override;
+       procedure OnRemoveChild( const Child_:TTreeNode ); override;
      public
        constructor Create; override;
        destructor Destroy; override;
        ///// プロパティ
-       property CameraUs :TGLBufferU<TCameraDat> read _CameraUs;
-       property GeometUs :TGLBufferU<TShaperDat> read _ShaperUs;
-       ///// メソッド
-       procedure Compile;
-       procedure Render;
+       property Cameras  :TDictionary<IGLCamera,Integer> read _Cameras ;
+       property Nodes    :TDictionary<IGLNode,Integer>   read _Nodes   ;
+       property CameraUs :TGLBufferU<TCameraDat>         read _CameraUs;
+       property NodesUs  :TGLBufferU<TNodeDat>           read _NodeUs  ;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -77,7 +124,9 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 implementation //############################################################### ■
 
-uses System.SysUtils, System.Classes;
+uses System.SysUtils, System.Classes,
+     LUX.GPU.OpenGL.Camera,
+     LUX.GPU.OpenGL.Shaper;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
@@ -96,15 +145,21 @@ begin
      Result := RootNode as TGLScener;
 end;
 
-procedure TGLNode.SetMove( const Move_:TSingleM4 );
-var
-   D :TShaperDat;
+//------------------------------------------------------------------------------
+
+function TGLNode.GetNodI :Integer;
 begin
-     _Move := Move_;
+     Result := Scener.Nodes[ Self ];
+end;
 
-     D.Move := _Move;
+function TGLNode.GetMove :TSingleM4;
+begin
+     Result := _Move;
+end;
 
-     Scener._ShaperUs[ Order ] := D;
+procedure TGLNode.SetMove( const Move_:TSingleM4 );
+begin
+     _Move := Move_;  RegBuf;
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
@@ -123,16 +178,63 @@ end;
 
 /////////////////////////////////////////////////////////////////////// メソッド
 
-procedure TGLNode.Draw;
+procedure TGLNode.RegBuf;
+var
+   D :TNodeDat;
 begin
+     D.Move := _Move;
 
+     Scener._NodeUs[ NodI ] := D;
 end;
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLWorld
+//------------------------------------------------------------------------------
+
+procedure TGLNode.Draw;
+var
+   I :Integer;
+begin
+     for I := 0 to ChildsN-1 do Childs[ I ].Draw;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLScener
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLScener.OnInsertChild( const Child_:TTreeNode );
+var
+   C :IGLCamera;
+   N :IGLNode;
+begin
+     if Child_ is TGLCamera then
+     begin
+          _Cameras.Add( Child_ as IGLCamera, _Cameras.Count );
+
+          _CameraUs.Count := _Cameras.Count;
+
+          for C in _Cameras.Keys do C.RegBuf;
+     end;
+
+     _Nodes.Add( Child_ as IGLNode, _Nodes.Count );
+
+     _NodeUs.Count := _Nodes.Count;
+
+     for N in _Nodes.Keys do N.RegBuf;
+
+     inherited;
+end;
+
+procedure TGLScener.OnRemoveChild( const Child_:TTreeNode );
+begin
+     if Child_ is TGLCamera then _Cameras.Remove( Child_ as IGLCamera );
+
+     _Nodes.Remove( Child_ as IGLNode );
+
+     inherited;
+end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
@@ -140,30 +242,22 @@ constructor TGLScener.Create;
 begin
      inherited;
 
+     _Cameras  := TDictionary<IGLCamera,Integer>.Create;
+     _Nodes    := TDictionary<IGLNode,Integer>.Create;
+
      _CameraUs := TGLBufferU<TCameraDat>.Create( GL_DYNAMIC_DRAW );
-     _ShaperUs := TGLBufferU<TShaperDat>.Create( GL_DYNAMIC_DRAW );
+     _NodeUs := TGLBufferU<TNodeDat>.Create( GL_DYNAMIC_DRAW );
 end;
 
 destructor TGLScener.Destroy;
 begin
-     _CameraUs.Free;
-     _ShaperUs.Free;
+     _CameraUs.DisposeOf;
+     _NodeUs.DisposeOf;
+
+     _Cameras .DisposeOf;
+     _Nodes   .DisposeOf;
 
      inherited;
-end;
-
-/////////////////////////////////////////////////////////////////////// メソッド
-
-procedure TGLScener.Compile;
-begin
-
-end;
-
-procedure TGLScener.Render;
-var
-   I :Integer;
-begin
-     for I := 0 to ChildsN-1 do Childs[ I ].Draw;
 end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
