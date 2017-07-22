2b5b7c6f15c24f6ae8d8669032253bc3ea04c747
luxidea <contact@luxidea.net>
2017-07-23 04:52:19 +0900

    TGLVarray クラス，IGLShader / IGLProgra インタフェースを追加。

--- » ATOM/LUX.GPU.OpenGL.Atom.pas
+++ » ATOM/LUX.GPU.OpenGL.Atom.pas
@@ -2,7 +2,8 @@
 
 interface //#################################################################### ■
 
-uses Winapi.OpenGL,
+uses System.Classes, System.SysUtils,
+     Winapi.OpenGL, Winapi.OpenGLext,
      LUX;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
@@ -34,6 +35,63 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        property ID :GLuint read GetID;
      end;
 
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLVarray
+
+     TGLVarray = class( TGLObject )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// メソッド
+       procedure Bind;
+       procedure Unbind;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLShader
+
+     IGLShader = interface( IGLObject )
+     ['{1E06B97A-6947-4960-8CD7-8FAD5CBCC546}']
+       ///// アクセス
+       function GetKind :GLenum;
+       function GetSource :TStringList;
+       function GetStatus :Boolean;
+       function GetErrors :TStringList;
+       ///// プロパティ
+       property Kind   :GLenum      read GetKind  ;
+       property Source :TStringList read GetSource;
+       property Status :Boolean     read GetStatus;
+       property Errors :TStringList read GetErrors;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLProgra
+
+     IGLProgra = interface( IGLObject )
+     ['{0B2FDEDE-30D3-439B-AC76-E61F9E028CD0}']
+     {protected}
+       ///// アクセス
+       function GetStatus :Boolean;
+       function GetErrors :TStringList;
+       ///// イベント
+       function GetOnLinked :TProc;
+       procedure SetOnLinked( const OnLinked_:TProc );
+     {public}
+       ///// プロパティ
+       property Status  :Boolean     read GetStatus ;
+       property Errors  :TStringList read GetErrors ;
+       ///// イベント
+       property OnLinked :TProc read GetOnLinked write SetOnLinked;
+       ///// メソッド
+       function glGetVertLoca( const Name_:String ) :GLuint;
+       function glGetBlocLoca( const Name_:String ) :GLuint;
+       function glGetUnifLoca( const Name_:String ) :GLuint;
+       procedure Attach( const Shader_:IGLShader );
+       procedure Detach( const Shader_:IGLShader );
+       procedure Link;
+       procedure Use;
+       procedure Unuse;
+     end;
+
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
 
 //var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
@@ -60,6 +118,40 @@ begin
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLVarray
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLVarray.Create;
+begin
+     inherited;
+
+     glGenVertexArrays( 1, @_ID );
+end;
+
+destructor TGLVarray.Destroy;
+begin
+     glDeleteVertexArrays( 1, @_ID );
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLVarray.Bind;
+begin
+     glBindVertexArray( _ID );
+end;
+
+procedure TGLVarray.Unbind;
+begin
+     glBindVertexArray( 0 );
+end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
