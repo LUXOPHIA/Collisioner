4bd42fe2924373daaed8bd9d710832ee1ec613b1
luxidea <contact@luxidea.net>
2017-07-23 07:09:58 +0900

    TGLMateryImagG クラスを追加。

--- VCL/LUX.GPU.OpenGL.Matery.VCL.pas
+++ VCL/LUX.GPU.OpenGL.Matery.VCL.pas
@@ -5,8 +5,8 @@ interface //####################################################################
 uses Winapi.OpenGL, Winapi.OpenGLext,
      LUX,
      LUX.GPU.OpenGL,
-     LUX.GPU.OpenGL.Imager,
-     LUX.GPU.OpenGL.Imager.VCL,
+     LUX.GPU.OpenGL.Atom.Imager,
+     LUX.GPU.OpenGL.Atom.Imager.VCL,
      LUX.GPU.OpenGL.Matery;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
@@ -35,6 +35,27 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      //-------------------------------------------------------------------------
 
      TGLMateryImag = class( TGLMatery, IGLMateryImag )
+     private
+     protected
+       _Sample :TGLSample;
+       _Imager :TGLImager2D_RGBA;
+       ///// アクセス
+       function GetSample :TGLSample;
+       function GetImager :TGLImager2D_RGBA;
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Sample :TGLSample        read GetSample;
+       property Imager :TGLImager2D_RGBA read GetImager;
+       ///// メソッド
+       procedure Use; override;
+       procedure Unuse; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMateryImagG
+
+     TGLMateryImagG = class( TGLMateryG, IGLMateryImag )
      private
      protected
        _Sample :TGLSample;
@@ -110,6 +131,58 @@ begin
 end;
 
 procedure TGLMateryImag.Unuse;
+begin
+     _Sample.Unuse( 0 );
+     _Imager.Unuse( 0 );
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMateryImagG
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+function TGLMateryImagG.GetSample :TGLSample;
+begin
+     Result := _Sample;
+end;
+
+function TGLMateryImagG.GetImager :TGLImager2D_RGBA;
+begin
+     Result := _Imager;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLMateryImagG.Create;
+begin
+     inherited;
+
+     _Sample  := TGLSample       .Create;
+     _Imager  := TGLImager2D_RGBA.Create;
+end;
+
+destructor TGLMateryImagG.Destroy;
+begin
+     _Sample.DisposeOf;
+     _Imager.DisposeOf;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLMateryImagG.Use;
+begin
+     inherited;
+
+     _Sample.Use( 0 );
+     _Imager.Use( 0 );
+end;
+
+procedure TGLMateryImagG.Unuse;
 begin
      _Sample.Unuse( 0 );
      _Imager.Unuse( 0 );
