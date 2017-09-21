91d763f503de379b3a99b01e189ae894f0b24a13
luxidea <contact@luxidea.net>
2017-09-22 04:29:20 +0900

    LUX.GPU.OpenGL.Matery.Preset ユニットを追加。

---
+++ LUX.GPU.OpenGL.Matery.Preset.pas
@@ -0,0 +1,327 @@
+﻿unit LUX.GPU.OpenGL.Matery.Preset;
+
+interface //#################################################################### ■
+
+uses System.SysUtils, System.UITypes,
+     Winapi.OpenGL, Winapi.OpenGLext,
+     LUX,
+     LUX.GPU.OpenGL,
+     LUX.GPU.OpenGL.Atom.Buffer.Unifor,
+     LUX.GPU.OpenGL.Matery;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMateryColor
+
+     TGLMateryColor = class( TGLMatery )
+     private
+     protected
+       _Color :TGLUnifor<TAlphaColorF>;
+       ///// アクセス
+       function GetColor :TAlphaColorF;
+       procedure SetColor( const Color_:TAlphaColorF );
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Color :TAlphaColorF read GetColor write SetColor;
+       ///// メソッド
+       procedure Use; override;
+       procedure Unuse; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMateryRGB
+
+     TGLMateryRGB = class( TGLMateryNor )
+     private
+     protected
+       _Ambient :TGLUnifor<TAlphaColorF>;
+       ///// アクセス
+       function GetAmbient :TAlphaColorF;
+       procedure SetAmbient( const Ambient_:TAlphaColorF );
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Ambient :TAlphaColorF read GetAmbient write SetAmbient;
+       ///// メソッド
+       procedure Use; override;
+       procedure Unuse; override;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMateryColor
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TGLMateryColor.GetColor :TAlphaColorF;
+begin
+     Result := _Color[ 0 ];
+end;
+
+procedure TGLMateryColor.SetColor( const Color_:TAlphaColorF );
+begin
+     _Color[ 0 ] := Color_;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLMateryColor.Create;
+begin
+     inherited;
+
+     _Color := TGLUnifor<TAlphaColorF>.Create( GL_STATIC_DRAW );
+     _Color.Count := 1;
+
+     with _ShaderV do
+     begin
+          with Source do
+          begin
+               BeginUpdate;
+                 Clear;
+
+                 Add( '#version 430' );
+
+                 Add( 'layout( std140 ) uniform TViewerScal{ layout( row_major ) mat4 _ViewerScal; };' );
+                 Add( 'layout( std140 ) uniform TCameraProj{ layout( row_major ) mat4 _CameraProj; };' );
+                 Add( 'layout( std140 ) uniform TCameraPose{ layout( row_major ) mat4 _CameraPose; };' );
+                 Add( 'layout( std140 ) uniform TShaperPose{ layout( row_major ) mat4 _ShaperPose; };' );
+
+                 Add( 'in vec4 _SenderPos;' );
+
+                 Add( 'out TSenderVF' );
+                 Add( '{' );
+                 Add( '  vec4 Pos;' );
+                 Add( '}' );
+                 Add( '_Result;' );
+
+                 Add( 'void main()' );
+                 Add( '{' );
+                 Add( '  _Result.Pos = _ShaperPose * _SenderPos;' );
+                 Add( '  gl_Position = _ViewerScal * _CameraProj * inverse( _CameraPose ) * _Result.Pos;' );
+                 Add( '}' );
+
+               EndUpdate;
+          end;
+
+          Assert( Status, Errors.Text );
+     end;
+
+     with _ShaderF do
+     begin
+          with Source do
+          begin
+               BeginUpdate;
+                 Clear;
+
+                 Add( '#version 430' );
+
+                 Add( 'layout( std140 ) uniform TMateryCol{ vec4 _MateryCol; };' );
+
+                 Add( 'in TSenderVF' );
+                 Add( '{' );
+                 Add( '  vec4 Pos;' );
+                 Add( '}' );
+                 Add( '_Sender;' );
+
+                 Add( 'out vec4 _ResultCol;' );
+
+                 Add( 'void main(){ _ResultCol = _MateryCol; }' );
+
+               EndUpdate;
+          end;
+
+          Assert( Status, Errors.Text );
+     end;
+
+     with _Engine do
+     begin
+          with Unifors do
+          begin
+               Add( 4{BinP}, 'TMateryCol'{Name} );
+          end;
+     end;
+
+     Color := TAlphaColorF.Create( 1, 0, 0, 1 );
+end;
+
+destructor TGLMateryColor.Destroy;
+begin
+     _Color.DisposeOf;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLMateryColor.Use;
+begin
+     inherited;
+
+     _Color.Use( 4 );
+end;
+
+procedure TGLMateryColor.Unuse;
+begin
+     _Color.Unuse( 4 );
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLMateryRGB
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TGLMateryRGB.GetAmbient :TAlphaColorF;
+begin
+     Result := _Ambient[ 0 ];
+end;
+
+procedure TGLMateryRGB.SetAmbient( const Ambient_:TAlphaColorF );
+begin
+     _Ambient[ 0 ] := Ambient_;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TGLMateryRGB.Create;
+begin
+     inherited;
+
+     _Ambient := TGLUnifor<TAlphaColorF>.Create( GL_STATIC_DRAW );
+     _Ambient.Count := 1;
+
+     with _ShaderV do
+     begin
+          with Source do
+          begin
+               BeginUpdate;
+                 Clear;
+
+                 Add( '#version 430' );
+
+                 Add( 'layout( std140 ) uniform TViewerScal{ layout( row_major ) mat4 _ViewerScal; };' );
+                 Add( 'layout( std140 ) uniform TCameraProj{ layout( row_major ) mat4 _CameraProj; };' );
+                 Add( 'layout( std140 ) uniform TCameraPose{ layout( row_major ) mat4 _CameraPose; };' );
+                 Add( 'layout( std140 ) uniform TShaperPose{ layout( row_major ) mat4 _ShaperPose; };' );
+
+                 Add( 'in vec4 _SenderPos;' );
+                 Add( 'in vec4 _SenderNor;' );
+
+                 Add( 'out TSenderVF' );
+                 Add( '{' );
+                 Add( '  vec4 Pos;' );
+                 Add( '  vec4 Nor;' );
+                 Add( '}' );
+                 Add( '_Result;' );
+
+                 Add( 'void main()' );
+                 Add( '{' );
+                 Add( '  _Result.Pos =                     _ShaperPose     * _SenderPos;' );
+                 Add( '  _Result.Nor = transpose( inverse( _ShaperPose ) ) * _SenderNor;' );
+                 Add( '  gl_Position = _ViewerScal * _CameraProj * inverse( _CameraPose ) * _Result.Pos;' );
+                 Add( '}' );
+
+               EndUpdate;
+          end;
+
+          Assert( Status, Errors.Text );
+     end;
+
+     with _ShaderF do
+     begin
+          with Source do
+          begin
+               BeginUpdate;
+                 Clear;
+
+                 Add( '#version 430' );
+
+                 Add( 'layout( std140 ) uniform TAmbient{ vec4 _Ambient; };' );
+
+                 Add( 'in TSenderVF' );
+                 Add( '{' );
+                 Add( '  vec4 Pos;' );
+                 Add( '  vec4 Nor;' );
+                 Add( '}' );
+                 Add( '_Sender;' );
+
+                 Add( 'out vec4 _ResultCol;' );
+
+                 Add( 'void main()' );
+                 Add( '{' );
+                 Add( '  _ResultCol = vec4( _Ambient.rgb + ( 1 + normalize( _Sender.Nor.xyz ) ) / 2, 1 );' );
+                 Add( '}' );
+
+               EndUpdate;
+          end;
+
+          Assert( Status, Errors.Text );
+     end;
+
+     with _Engine do
+     begin
+          with Unifors do
+          begin
+               Add( 4{BinP}, 'TAmbient'{Name} );
+          end;
+     end;
+
+     Ambient := TAlphaColorF.Create( 0, 0, 0 );
+end;
+
+destructor TGLMateryRGB.Destroy;
+begin
+     inherited;
+
+     _Ambient.DisposeOf;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TGLMateryRGB.Use;
+begin
+     inherited;
+
+     _Ambient.Use( 4 );
+end;
+
+procedure TGLMateryRGB.Unuse;
+begin
+     _Ambient.Unuse( 4 );
+
+     inherited;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
