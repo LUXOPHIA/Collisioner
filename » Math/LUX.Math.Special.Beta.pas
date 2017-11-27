e3fc1239eef4040b55e626d79d6d7461007ebfa9
luxidea <contact@luxidea.net>
2017-11-27 21:35:33 +0900

    正則/不完全ベータ関数 IncBeta, RegIncBeta を追加。

--- » Math/LUX.Math.Special.Beta.pas
+++ » Math/LUX.Math.Special.Beta.pas
@@ -2,7 +2,8 @@
 
 interface //#################################################################### ■
 
-uses LUX;
+uses LUX, LUX.D1,
+     LUX.Math.Special;
 
 //type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
@@ -22,9 +23,23 @@ uses LUX;
 
 function LnBeta( const X_,Y_:Single ) :Single; overload;
 function LnBeta( const X_,Y_:Double ) :Double; overload;
+function LnBeta( const X_,Y_:TdSingle ) :TdSingle; overload;
+function LnBeta( const X_,Y_:TdDouble ) :TdDouble; overload;
 
 function Beta( const X_,Y_:Single ) :Single; overload;
 function Beta( const X_,Y_:Double ) :Double; overload;
+function Beta( const X_,Y_:TdSingle ) :TdSingle; overload;
+function Beta( const X_,Y_:TdDouble ) :TdDouble; overload;
+
+function IncBeta( const X_,A_,B_:Single ) :Single; overload;
+function IncBeta( const X_,A_,B_:Double ) :Double; overload;
+function IncBeta( const X_,A_,B_:TdSingle ) :TdSingle; overload;
+function IncBeta( const X_,A_,B_:TdDouble ) :TdDouble; overload;
+
+function RegIncBeta( const X_,A_,B_:Single ) :Single; overload;
+function RegIncBeta( const X_,A_,B_:Double ) :Double; overload;
+function RegIncBeta( const X_,A_,B_:TdSingle ) :TdSingle; overload;
+function RegIncBeta( const X_,A_,B_:TdDouble ) :TdDouble; overload;
 
 implementation //############################################################### ■
 
@@ -61,6 +76,16 @@ begin
      Result := LnGammaP( X_ ) + LnGammaP( Y_ ) - LnGammaP( X_ + Y_ );
 end;
 
+function LnBeta( const X_,Y_:TdSingle ) :TdSingle;
+begin
+     Result := LnGammaP( X_ ) + LnGammaP( Y_ ) - LnGammaP( X_ + Y_ );
+end;
+
+function LnBeta( const X_,Y_:TdDouble ) :TdDouble;
+begin
+     Result := LnGammaP( X_ ) + LnGammaP( Y_ ) - LnGammaP( X_ + Y_ );
+end;
+
 //------------------------------------------------------------------------------
 
 function Beta( const X_,Y_:Single ) :Single;
@@ -71,6 +96,60 @@ end;
 function Beta( const X_,Y_:Double ) :Double;
 begin
      Result := Exp( LnBeta( X_, Y_ ) );
+end;
+
+function Beta( const X_,Y_:TdSingle ) :TdSingle;
+begin
+     Result := Exp( LnBeta( X_, Y_ ) );
+end;
+
+function Beta( const X_,Y_:TdDouble ) :TdDouble;
+begin
+     Result := Exp( LnBeta( X_, Y_ ) );
+end;
+
+//------------------------------------------------------------------------------
+
+function IncBeta( const X_,A_,B_:Single ) :Single;
+begin
+     Result := Power( X_, A_ ) / A_ * HypGeo21( A_, 1 - B_, A_ + 1, X_ );
+end;
+
+function IncBeta( const X_,A_,B_:Double ) :Double;
+begin
+     Result := Power( X_, A_ ) / A_ * HypGeo21( A_, 1 - B_, A_ + 1, X_ );
+end;
+
+function IncBeta( const X_,A_,B_:TdSingle ) :TdSingle;
+begin
+     Result := Power( X_, A_ ) / A_ * HypGeo21( A_, 1 - B_, A_ + 1, X_ );
+end;
+
+function IncBeta( const X_,A_,B_:TdDouble ) :TdDouble;
+begin
+     Result := Power( X_, A_ ) / A_ * HypGeo21( A_, 1 - B_, A_ + 1, X_ );
+end;
+
+//------------------------------------------------------------------------------
+
+function RegIncBeta( const X_,A_,B_:Single ) :Single;
+begin
+     Result := IncBeta( X_, A_, B_ ) / Beta( A_, B_ );
+end;
+
+function RegIncBeta( const X_,A_,B_:Double ) :Double;
+begin
+     Result := IncBeta( X_, A_, B_ ) / Beta( A_, B_ );
+end;
+
+function RegIncBeta( const X_,A_,B_:TdSingle ) :TdSingle;
+begin
+     Result := IncBeta( X_, A_, B_ ) / Beta( A_, B_ );
+end;
+
+function RegIncBeta( const X_,A_,B_:TdDouble ) :TdDouble;
+begin
+     Result := IncBeta( X_, A_, B_ ) / Beta( A_, B_ );
 end;
 
 //############################################################################## □
