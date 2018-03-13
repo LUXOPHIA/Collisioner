2b8fd5c9bb97e6c57dd3593e7ed0417710ded8f2
luxidea <contact@luxidea.net>
2018-03-13 18:14:54 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX/' changes from 55f9a5b..fee6b50
    fee6b50 LUX.Data.Lattice.T1.D1/2 ユニットを追加。
    6a3018b Merge commit '238b93dc7fc7e5fd43f72dc6b4e623a57c9a93f8'
    d963ca9 Merge commit 'de775b7dccc08eab78e63fe740f8afb580a3e8cc' into feature
    0b60a5f Merge commit '4d1e26a42095d4c3a01ecd007f46b2562fd213df' into feature
    96e1134 Merge commit '9052d1f1d0a125f597925fa2b9fd082639eda83f' into future_nakayama
    d3c402a Merge commit '988731f2f70fa6ce841b37d95c4ca2d75c1f6d83' into nakayama
    988731f Squashed '_LIBRARY/LUXOPHIA/LUX.Lattice/' changes from 0dc673aa..5c30c6f5
    6d00c8a Merge commit 'a45acf59aaf1f7de2d6ee84bb278b7ea00b811af' into nakayama
    ed3d983 Merge commit '81e820d33933894f82fc896a31a081c876c61b37' into nakayama
    d82d3d4 Merge commit 'b0184421c187f4dada566659ed7a7b8a03b66b0e' into nakayama
    5c22cd5 Merge commit '00b2b7f5d4637edcb8974bfcc95a6c76f27d6a5d' into nakayama
    e4a282e 逆行列メソッド Inverse を追加。
    50720ea Merge commit '32ac0967ce001b68e03b649a80f9dbc6b5536576' into nakayama
    32ac096 Squashed '_LIBRARY/LUXOPHIA/LUX.Lattice/' changes from 6eb976e..0dc673a
    7692342 Add '_LIBRARY/LUXOPHIA/LUX.Lattice/' from commit '6eb976eabd1ff0344623fc224a83f18096434f97'
    6eb976e LUX.Matrix.L4 → LUX.M4 へ代替。
    3943dc0 バグ修正。
    24aaeae LUX.Lattice.T2.D1 ユニットを追加。
    e095a06 LUX.Map → LUX.Lattice へ改名。
    e45af67 README.md edited online with Bitbucket
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX
    git-subtree-split: fee6b50952692672ce9f90b134b8eb516c123f17

---
+++ Data/Lattice/T2/LUX.Data.Lattice.T2.D1.pas
@@ -0,0 +1,160 @@
+﻿unit LUX.Data.Lattice.T2.D1;
+
+interface //#################################################################### ■
+
+uses LUX, LUX.D1, LUX.D2,
+     LUX.M4,
+     LUX.Data.Lattice.T2,
+     LUX.Curve.T2.D1;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleGridMap2T<T_Item>
+
+     TSingleGridMap2T = class( TGridArray2D<Single> )
+     private
+     protected
+     public
+       constructor Create; overload;
+       constructor Create( const BricX_,BricY_:Integer ); overload;
+       destructor Destroy; override;
+       ///// メソッド
+       procedure Patch( const T_:TSingle2D; out M_:TSingleM4 ); overload;
+       procedure Patch( const T_:TSingle2D; out M_:TdSingleM4 ); overload;
+       function Interp( const T_:TSingle2D ) :Single; overload;
+       function Interp( const T_:TdSingle2D ) :TdSingle; overload;
+       function Nabla( const T_:TSingle2D ) :TSingle2D;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses System.Math;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleGridMap2T
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TSingleGridMap2T.Create;
+begin
+     Create( 2, 2 );
+end;
+
+constructor TSingleGridMap2T.Create( const BricX_,BricY_:Integer );
+begin
+     Create( BricX_, BricY_, 2, 2 );
+end;
+
+destructor TSingleGridMap2T.Destroy;
+begin
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TSingleGridMap2T.Patch( const T_:TSingle2D; out M_:TSingleM4 );
+var
+   X1, X2, X3, X4,
+   Y1, Y2, Y3, Y4 :Integer;
+begin
+     X2 := Floor( T_.X );  X1 := X2 - 1;  X3 := X2 + 1;  X4 := X3 + 1;
+     Y2 := Floor( T_.Y );  Y1 := Y2 - 1;  Y3 := Y2 + 1;  Y4 := Y3 + 1;
+
+     with M_ do
+     begin
+          _11 := Grids[ X1, Y1 ];  _12 := Grids[ X2, Y1 ];  _13 := Grids[ X3, Y1 ];  _14 := Grids[ X4, Y1 ];
+          _21 := Grids[ X1, Y2 ];  _22 := Grids[ X2, Y2 ];  _23 := Grids[ X3, Y2 ];  _24 := Grids[ X4, Y2 ];
+          _31 := Grids[ X1, Y3 ];  _32 := Grids[ X2, Y3 ];  _33 := Grids[ X3, Y3 ];  _34 := Grids[ X4, Y3 ];
+          _41 := Grids[ X1, Y4 ];  _42 := Grids[ X2, Y4 ];  _43 := Grids[ X3, Y4 ];  _44 := Grids[ X4, Y4 ];
+     end;
+end;
+
+procedure TSingleGridMap2T.Patch( const T_:TSingle2D; out M_:TdSingleM4 );
+var
+   X1, X2, X3, X4,
+   Y1, Y2, Y3, Y4 :Integer;
+begin
+     X2 := Floor( T_.X );  X1 := X2 - 1;  X3 := X2 + 1;  X4 := X3 + 1;
+     Y2 := Floor( T_.Y );  Y1 := Y2 - 1;  Y3 := Y2 + 1;  Y4 := Y3 + 1;
+
+     with M_ do
+     begin
+          _11 := Grids[ X1, Y1 ];  _12 := Grids[ X2, Y1 ];  _13 := Grids[ X3, Y1 ];  _14 := Grids[ X4, Y1 ];
+          _21 := Grids[ X1, Y2 ];  _22 := Grids[ X2, Y2 ];  _23 := Grids[ X3, Y2 ];  _24 := Grids[ X4, Y2 ];
+          _31 := Grids[ X1, Y3 ];  _32 := Grids[ X2, Y3 ];  _33 := Grids[ X3, Y3 ];  _34 := Grids[ X4, Y3 ];
+          _41 := Grids[ X1, Y4 ];  _42 := Grids[ X2, Y4 ];  _43 := Grids[ X3, Y4 ];  _44 := Grids[ X4, Y4 ];
+     end;
+end;
+
+function TSingleGridMap2T.Interp( const T_:TSingle2D ) :Single;
+var
+   M :TSingleM4;
+   T :TSingle2D;
+begin
+     Patch( T_, M );
+
+     T.X := Frac( T_.X );
+     T.Y := Frac( T_.Y );
+
+     Result := BSplin4( M, T );
+end;
+
+function TSingleGridMap2T.Interp( const T_:TdSingle2D ) :TdSingle;
+var
+   M :TdSingleM4;
+   T :TdSingle2D;
+begin
+     Patch( T_.o, M );
+
+     T.X.o := Frac( T_.X.o );  T.X.d := T_.X.d;
+     T.Y.o := Frac( T_.Y.o );  T.Y.d := T_.Y.d;
+
+     Result := BSplin4( M, T );
+end;
+
+function TSingleGridMap2T.Nabla( const T_:TSingle2D ) :TSingle2D;
+var
+   M :TdSingleM4;
+   T :TdSingle2D;
+begin
+     Patch( T_, M );
+
+     T.X.o := Frac( T_.X );
+     T.Y.o := Frac( T_.Y );
+
+     T.d := TSingle2D.Create( 1, 0 );
+
+     Result.X := BSplin4( M, T ).d;
+
+     T.d := TSingle2D.Create( 0, 1 );
+
+     Result.Y := BSplin4( M, T ).d;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
