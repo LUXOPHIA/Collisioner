ff47df06b8c1e3410d35e2056c4001de1ae20a61
luxidea <contact@luxidea.net>
2018-04-29 18:44:14 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX/' changes from 69f52627..2fd0e771
    2fd0e771 BricsN → CellsX プロパティへ改名。 Grids[] → Poins[] プロパティへ改名。 TGridArray2D → TPoinArray2D クラスへ改名。
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX
    git-subtree-split: 2fd0e7716fcda1cce4fbdf50e568f87f1fccabf7

--- Data/Lattice/T2/LUX.Data.Lattice.T2.D1.pas
+++ Data/Lattice/T2/LUX.Data.Lattice.T2.D1.pas
@@ -15,7 +15,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleGridMap2T<T_Item>
 
-     TSingleGridMap2T = class( TGridArray2D<Single> )
+     TSingleGridMap2T = class( TPoinArray2D<Single> )
      private
      protected
      public
@@ -80,10 +80,10 @@ begin
 
      with M_ do
      begin
-          _11 := Grids[ X1, Y1 ];  _12 := Grids[ X2, Y1 ];  _13 := Grids[ X3, Y1 ];  _14 := Grids[ X4, Y1 ];
-          _21 := Grids[ X1, Y2 ];  _22 := Grids[ X2, Y2 ];  _23 := Grids[ X3, Y2 ];  _24 := Grids[ X4, Y2 ];
-          _31 := Grids[ X1, Y3 ];  _32 := Grids[ X2, Y3 ];  _33 := Grids[ X3, Y3 ];  _34 := Grids[ X4, Y3 ];
-          _41 := Grids[ X1, Y4 ];  _42 := Grids[ X2, Y4 ];  _43 := Grids[ X3, Y4 ];  _44 := Grids[ X4, Y4 ];
+          _11 := Poins[ X1, Y1 ];  _12 := Poins[ X2, Y1 ];  _13 := Poins[ X3, Y1 ];  _14 := Poins[ X4, Y1 ];
+          _21 := Poins[ X1, Y2 ];  _22 := Poins[ X2, Y2 ];  _23 := Poins[ X3, Y2 ];  _24 := Poins[ X4, Y2 ];
+          _31 := Poins[ X1, Y3 ];  _32 := Poins[ X2, Y3 ];  _33 := Poins[ X3, Y3 ];  _34 := Poins[ X4, Y3 ];
+          _41 := Poins[ X1, Y4 ];  _42 := Poins[ X2, Y4 ];  _43 := Poins[ X3, Y4 ];  _44 := Poins[ X4, Y4 ];
      end;
 end;
 
@@ -97,10 +97,10 @@ begin
 
      with M_ do
      begin
-          _11 := Grids[ X1, Y1 ];  _12 := Grids[ X2, Y1 ];  _13 := Grids[ X3, Y1 ];  _14 := Grids[ X4, Y1 ];
-          _21 := Grids[ X1, Y2 ];  _22 := Grids[ X2, Y2 ];  _23 := Grids[ X3, Y2 ];  _24 := Grids[ X4, Y2 ];
-          _31 := Grids[ X1, Y3 ];  _32 := Grids[ X2, Y3 ];  _33 := Grids[ X3, Y3 ];  _34 := Grids[ X4, Y3 ];
-          _41 := Grids[ X1, Y4 ];  _42 := Grids[ X2, Y4 ];  _43 := Grids[ X3, Y4 ];  _44 := Grids[ X4, Y4 ];
+          _11 := Poins[ X1, Y1 ];  _12 := Poins[ X2, Y1 ];  _13 := Poins[ X3, Y1 ];  _14 := Poins[ X4, Y1 ];
+          _21 := Poins[ X1, Y2 ];  _22 := Poins[ X2, Y2 ];  _23 := Poins[ X3, Y2 ];  _24 := Poins[ X4, Y2 ];
+          _31 := Poins[ X1, Y3 ];  _32 := Poins[ X2, Y3 ];  _33 := Poins[ X3, Y3 ];  _34 := Poins[ X4, Y3 ];
+          _41 := Poins[ X1, Y4 ];  _42 := Poins[ X2, Y4 ];  _43 := Poins[ X3, Y4 ];  _44 := Poins[ X4, Y4 ];
      end;
 end;
 
