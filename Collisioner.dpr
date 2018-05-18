9f65adab8ed437f62b0c5f369ea25baf3eabb8d7
luxidea <contact@luxidea.net>
2018-05-18 22:25:50 +0900

    no message

--- Collisioner.dpr
+++ Collisioner.dpr
@@ -79,7 +79,14 @@ uses
   LUX.GPU.OpenGL.Atom.Imager.D3.Preset in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\Atom\Imager\D3\_FMX\LUX.GPU.OpenGL.Atom.Imager.D3.Preset.pas',
   LUX.GPU.OpenGL.Atom.Textur.D1.Preset in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\Atom\Textur\D1\LUX.GPU.OpenGL.Atom.Textur.D1.Preset.pas',
   LUX.GPU.OpenGL.Atom.Textur.D2.Preset in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\Atom\Textur\D2\LUX.GPU.OpenGL.Atom.Textur.D2.Preset.pas',
-  LUX.GPU.OpenGL.Atom.Textur.D3.Preset in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\Atom\Textur\D3\LUX.GPU.OpenGL.Atom.Textur.D3.Preset.pas';
+  LUX.GPU.OpenGL.Atom.Textur.D3.Preset in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\Atom\Textur\D3\LUX.GPU.OpenGL.Atom.Textur.D3.Preset.pas',
+  LUX.Color.Grid.D1 in '_LIBRARY\LUXOPHIA\LUX\Color\Grid\_FMX\LUX.Color.Grid.D1.pas',
+  LUX.Color.Grid.D2.Preset in '_LIBRARY\LUXOPHIA\LUX\Color\Grid\D2\_FMX\LUX.Color.Grid.D2.Preset.pas',
+  LUX.Color.Grid.D1.Preset in '_LIBRARY\LUXOPHIA\LUX\Color\Grid\D1\_FMX\LUX.Color.Grid.D1.Preset.pas',
+  LUX.Color.Format.HDR in '_LIBRARY\LUXOPHIA\LUX\Color\Format\LUX.Color.Format.HDR.pas',
+  LUX.Color in '_LIBRARY\LUXOPHIA\LUX\Color\LUX.Color.pas',
+  LUX.Color.Grid.D3 in '_LIBRARY\LUXOPHIA\LUX\Color\Grid\_FMX\LUX.Color.Grid.D3.pas',
+  LUX.Color.Grid.D2 in '_LIBRARY\LUXOPHIA\LUX\Color\Grid\_FMX\LUX.Color.Grid.D2.pas';
 
 {$R *.res}
 
